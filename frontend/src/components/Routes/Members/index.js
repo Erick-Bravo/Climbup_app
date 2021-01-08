08de71b0b4ad108dbdbade7d74dd0feec1027878
Erick Bravo <erickbrvo@gmail.com>
2021-01-07 23:06:01 -0800

    some styling to the GroupPage:

--- frontend/src/components/Routes/Members/index.js
+++ frontend/src/components/Routes/Members/index.js
@@ -10,15 +10,6 @@ const Members = ({ group }) => {
     // const GroupMembers = memberTables.filter(memberTable => memberTable.groupId === parseInt(groupId, 10))
     const GroupMembers = group.Users
 
-
-
-    // const members = useSelector(fullReduxState => fullReduxState.groups.Users)
-
-
-    // //Randomized 4 Members
-    // //Sometimes its only 3, which is weird.
-    // Random 4 (or 3) members
-
     const randomBoxGen = (groupMembers) => {
         let randomMembers = [];
         let randomNumCheck = [];
@@ -31,16 +22,21 @@ const Members = ({ group }) => {
                 randomMembers.push(groupMembers[randomMemberIndex])
             }
         }
-        console.log(randomMembers)
 
         return (
             <div id="random-members-box">
+                <div>
+                    <p>{`Currently ${GroupMembers.length} Members`}</p>
+                    <NavLink to={`/groups/${groupId}/members`}>See All Members</NavLink>
+                </div>
                 {randomMembers.map((member) => {
                     return (
-                        <div id="member">
-                            <img alt="null" src={member.photoUrl} />
-                            <h3>{member.username}</h3>
-                        </div>
+                        <>
+                            <div id="member">
+                                <img alt="null" src={member.photoUrl} />
+                                <h3>{member.username}</h3>
+                            </div>
+                        </>
                     )
                 })}
             </div>
@@ -55,14 +51,6 @@ const Members = ({ group }) => {
                 <>
                     {randomBoxGen(GroupMembers)}
                 </>}
-            {GroupMembers &&
-                <div>
-                    <p>{`Currently ${GroupMembers.length} Members`}</p>
-                    <NavLink to={`/groups/${groupId}/members`}>See All Members</NavLink>
-                </div>}
-
-
-
         </div>
     )
 }
